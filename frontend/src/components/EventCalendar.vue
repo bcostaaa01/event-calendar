7423984f14cf9029459d15ff9b5316d434ad7cb5
Bruno Costa <bcostinha01@gmail.com>
2023-01-11 11:08:56 +0000

    Create Vue app, install vue-cal, render basic calendar component

---
+++ frontend/src/components/EventCalendar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <vue-cal></vue-cal> 
+    </div>
+</template>
+
+<script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
+export default {
+    name: "EventCalendar",
+    components: {
+        VueCal
+    }
+}
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <EventCalendar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCalendar from "@/components/EventCalendar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    EventCalendar,
+  },
+};
+</script>
