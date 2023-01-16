d087d420e1d0a67010e831e6f002c1ec9475319f
Bruno Costa <bcostinha01@gmail.com>
2023-01-16 19:15:48 +0000

    Add description of the app on the About view, add gif to About view

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Sports Events Calendar</h1>
+    <p>
+      This application shows data from sports events fetched from a backend of
+      JSON data. It was built using Vue 3, Vuex, NodeJS and ExpressJS.
+    </p>
+    <img class="gif" src="https://j.gifs.com/y0wkkB.gif" alt="champions league gif" />
   </div>
 </template>
+
+<style scoped>
+.gif {
+  width: 60%;
+  height: 60%;
+}
+</style>
